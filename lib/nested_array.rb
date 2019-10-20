703457dc6ef0eaf7e69e6ac82580afd69aa84f7c
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-20 01:39:50 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,6 +38,8 @@ end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
+  matrix[row][column]
+    
 end
 
 def matrix_update(matrix, row, column, new_value)
